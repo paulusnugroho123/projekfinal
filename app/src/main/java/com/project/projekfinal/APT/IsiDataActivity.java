04d3218cb41e2191430f3c24f8a9674b9ba1dc8e
paulusnugroho123 <paulusagata123@gmail.com>
2020-06-22 21:40:46 +0700

    update

--- app/src/main/java/com/project/projekfinal/APT/IsiDataActivity.java
+++ app/src/main/java/com/project/projekfinal/APT/IsiDataActivity.java
@@ -12,5 +12,6 @@ public class IsiDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_isi_data);
+        getSupportActionBar().setTitle("Verifikasi Data Customer");
     }
 }
