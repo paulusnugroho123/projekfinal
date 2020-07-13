ad8c2a80fb594a45625a48c175ac8344f4ba811f
paulusnugroho123 <paulusagata123@gmail.com>
2020-07-13 19:02:08 +0700

    update16

---
+++ app/src/main/java/com/project/projekfinal/SeksiKI/ValidasiRateSeksiKIFragment.java
@@ -0,0 +1,29 @@
+package com.project.projekfinal.SeksiKI;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.project.projekfinal.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ValidasiRateSeksiKIFragment extends Fragment {
+
+    public ValidasiRateSeksiKIFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_validasi_rate_seksi_k_i, container, false);
+    }
+}
